d6974fb4bc58e663683102f5e1063f4636e87aae
Todor <todor_nikolov@ymail.com>
2017-12-18 21:09:45 +0200

    https://github.com/Nikoltod/Hello-Go.git

--- server/server.go
+++ server/server.go
@@ -5,10 +5,12 @@ import (
 	"net/http"
 )
 
+//Simple handler for incoming requests using the 'io' package
 func ws(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Hello world!")
 }
 
+//Handling the requests on '/' using port 8080
 func main() {
 	http.HandleFunc("/", ws)
 	http.ListenAndServe(":8080", nil)
