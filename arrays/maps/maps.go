0e79dc57f48af27e9e9e152ec95a15cc2fc5043c
Todor <todor_nikolov@ymail.com>
2018-01-08 22:39:44 +0200

    Maps literal declaration

--- arrays/maps/maps.go
+++ arrays/maps/maps.go
@@ -6,10 +6,7 @@ type Square struct {
 	x, y float64
 }
 
-func Init() {
-
-	fmt.Printf("\n Maps example in GoLang \n")
-
+func mapSample() {
 	var a = make(map[string]Square)
 
 	a["Alpha"] = Square{
@@ -23,4 +20,31 @@ func Init() {
 	// The zero value of a map is nil. A nil map has no keys, nor can keys be added.
 
 	// The make function returns a map of the given type, initialized and ready for use.
+}
+
+func mapLiterals() {
+	var m = map[string]Square{
+		"Alpha": Square{
+			50.5162,
+			23.5561,
+		},
+		"Beta": Square{
+			52.5127,
+			23.5561,
+		},
+	}
+
+	fmt.Println(m)
+}
+
+func Init() {
+
+	fmt.Printf("\n Maps example in GoLang \n")
+
+	fmt.Printf("\n Map declaration \n")
+	mapSample()
+
+	fmt.Printf("\n Map literal declaration \n")
+	mapLiterals()
+
 }
