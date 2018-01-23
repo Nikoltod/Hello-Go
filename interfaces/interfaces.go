daf13f4232282790400c93dee0be38fa4bdf46a9
Todor <todor_nikolov@ymail.com>
2018-01-23 21:37:33 +0200

    Added interfaces demo

---
+++ interfaces/interfaces.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Abser interface {
+	Abs() float64
+}
+
+type Vertex struct {
+	X, Y float64
+}
+
+type MyFloat float64
+
+func (f MyFloat) Abs() float64 {
+	if f < 0 {
+		return float64(-f)
+	}
+	return float64(f)
+}
+
+func (v *Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func main() {
+	var a Abser
+	f := MyFloat(-math.Sqrt2)
+	v := Vertex{3, 4}
+
+	a = f // a MyFloat implements Abser
+	fmt.Printf("Method 1 with float value reciever : %v", a.Abs())
+
+	fmt.Println()
+
+	a = &v // a *Vertex implements Abser
+	fmt.Printf("Method 2 with pointer reciever : %v", a.Abs())
+
+}
+
+// An interface type is defined as a set of method signatures.
+// A value of interface type can hold any value that implements those methods.
